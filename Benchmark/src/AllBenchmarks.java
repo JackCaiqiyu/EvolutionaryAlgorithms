d3f48a9a9263c2820181610bcac50a7068a068de
framg <framorgar@gmail.com>
2016-05-02 20:50:34 +0200

    Benchmark framework created. CEC13 updated.

--- Benchmark/src/AllBenchmarks.java
+++ Benchmark/src/AllBenchmarks.java
@@ -2,5 +2,25 @@
  * Created by framg on 26/03/2016.
  */
 public abstract class AllBenchmarks {
+    protected int DIM;
+    protected int FUN;
+    protected int n_FUN;
+
+    AllBenchmarks(int DIM, int FUN, int n_FUN){
+        this.DIM = DIM;
+        this.FUN = FUN;
+        this.n_FUN = n_FUN;
+    }
+
+
+    public int nFunctions(){
+     return n_FUN;
+    }
+
     public abstract double f(double[] x);
+    public abstract double bias();
+    public abstract double lbound();
+    public abstract double ubound();
+
+
 }
